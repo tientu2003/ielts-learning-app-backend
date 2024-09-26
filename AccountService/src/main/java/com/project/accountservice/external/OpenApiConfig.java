a0f0c6a30cf1ff8e65de0f9bcca1a8a2a4fe0a28
tientu <tientu2003@gmail.com>
2024-09-27 03:22:58 +0700

    Add Rest API Docs

---
+++ AccountService/src/main/java/com/project/accountservice/external/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.project.accountservice.external;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Account Service")
+                        .description("This is Rest API for Account Service")
+                        .version("v0.1")
+                        .license(new License().name("Apache 2.0"))
+                );
+    }
+}
