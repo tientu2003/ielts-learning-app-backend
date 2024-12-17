eeb684c2705a47d02a0bd237ad57360405efc72d
tientu <tientu2003@gmail.com>
2024-12-17 08:16:17 +0700

    update

--- listening-service/src/main/java/com/project/listeningservice/external/data/ListeningAnswer.java
+++ listening-service/src/main/java/com/project/listeningservice/external/data/ListeningAnswer.java
@@ -1,4 +1,16 @@
 package com.project.listeningservice.external.data;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class ListeningAnswer {
+    String id;
+
+    String testName;
+
+    List<String> answers;
 }
