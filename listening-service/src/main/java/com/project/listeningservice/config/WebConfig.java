86d21c36a39920163050ba39aff5ac854c7d334f
tientu <tientu2003@gmail.com>
2024-12-31 14:30:00 +0700

    deploy: reading-service

--- listening-service/src/main/java/com/project/listeningservice/config/WebConfig.java
+++ listening-service/src/main/java/com/project/listeningservice/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.project.listeningservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
