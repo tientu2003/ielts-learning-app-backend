91316f18fd1f198453cf7f111aaaa345a0b688d1
tientu <tientu2003@gmail.com>
2024-12-31 21:16:49 +0700

    deploy-hotfix: reading-service

--- reading-service/src/main/java/com/project/readingservice/config/SecurityConfiguration.java
+++ reading-service/src/main/java/com/project/readingservice/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http .cors(Customizer.withDefaults())
+        http .cors(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers("public/api/reading/**",
                             "swagger-ui/**",
