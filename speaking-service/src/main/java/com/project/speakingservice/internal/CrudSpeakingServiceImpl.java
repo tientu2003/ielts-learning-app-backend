1e72a6ce78e5ad52f87a28444b3517d0537b8cfe
tientu <tientu2003@gmail.com>
2025-05-20 08:34:05 +0700

    hot-fix: speaking-service

--- speaking-service/src/main/java/com/project/speakingservice/internal/CrudSpeakingServiceImpl.java
+++ speaking-service/src/main/java/com/project/speakingservice/internal/CrudSpeakingServiceImpl.java
@@ -2,7 +2,6 @@ package com.project.speakingservice.internal;
 
 import com.project.common.dto.BasicExamDTO;
 import com.project.speakingservice.CrudSpeakingService;
-import com.project.speakingservice.external.IdQuestion;
 import com.project.speakingservice.external.data.SpeakingExam;
 import com.project.speakingservice.internal.entity.data.MongoSpeakingExam;
 import com.project.speakingservice.internal.entity.data.MongoSpeakingExamRepository;
@@ -10,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,56 +19,17 @@ public class CrudSpeakingServiceImpl implements CrudSpeakingService {
 
     @Override
     public List<BasicExamDTO> listAllSpeakingExam() {
-
-        // type 1 only have questionsOne, topic 
-        List<MongoSpeakingExam> partOne = mongoSpeakingExamRepository.findAllByType(1);
-        // type 2 only have questionsTwo, questionsThree, topic
-        List<MongoSpeakingExam> partTwo = mongoSpeakingExamRepository.findAllByType(2);
-
-        List<BasicExamDTO> result = new ArrayList<>();
-
-        partOne.parallelStream().forEach(one -> {
-            partTwo.parallelStream().forEach(two -> {
-                result.add(BasicExamDTO.builder()
-                        .id(one.getId() + "_" + two.getId())
-                        .testName("Speaking Test: " + one.getTopic() + " - " + two.getTopic())
-                        .topics(List.of(one.getTopic(), two.getTopic()))
-                        .build());
-            });
-        });
-        return result;
+        List<MongoSpeakingExam> data = mongoSpeakingExamRepository.findAll();
+        return data.parallelStream().map(MongoSpeakingExam::basicExamDTO).toList();
     }
-    
-    
 
     @Override
     public SpeakingExam getSpeakingExamById(String id) {
-        String[] ids = id.split("_");
-        MongoSpeakingExam partOne = mongoSpeakingExamRepository.findById(ids[0]).orElseThrow();
-        MongoSpeakingExam partTwo = mongoSpeakingExamRepository.findById(ids[1]).orElseThrow();
-
-        return SpeakingExam.builder()
-                .id(id)
-                .partOne(partOne.getQuestionsOne().stream()
-                        .map(q -> IdQuestion.builder()
-                                .number(q.getNumber())
-                                .topic(partOne.getTopic())
-                                .question(q.getQuestion())
-                                .build())
-                        .toList())
-                .partTwo(IdQuestion.builder()
-                        .number(1)
-                        .topic(partTwo.getTopic())
-                        .question(partTwo.getQuestionsTwo())
-                        .build())
-                .partThree(partTwo.getQuestionsThree().stream()
-                        .map(q -> IdQuestion.builder()
-                                .number(q.getNumber())
-                                .topic(partTwo.getTopic())
-                                .question(q.getQuestion())
-                                .build())
-                        .toList())
-                .build();
+        MongoSpeakingExam exam = mongoSpeakingExamRepository.findById(id).orElseThrow(null);
+        if(exam == null) {
+            return null;
+        }
+        return exam.toSpeakingExam();
     }
 
     @Override
--- speaking-service/src/main/java/com/project/speakingservice/internal/entity/data/MongoSpeakingExam.java
+++ speaking-service/src/main/java/com/project/speakingservice/internal/entity/data/MongoSpeakingExam.java
@@ -1,5 +1,8 @@
 package com.project.speakingservice.internal.entity.data;
 
+import com.project.common.dto.BasicExamDTO;
+import com.project.speakingservice.external.IdQuestion;
+import com.project.speakingservice.external.data.SpeakingExam;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -39,5 +42,38 @@ public class MongoSpeakingExam {
         @Field("question")
         private String question;
     }
+
+    public BasicExamDTO basicExamDTO(){
+        return BasicExamDTO.builder()
+                .id(this.id)
+                .testName("Speaking Test: " + this.getTopic())
+                .topics(List.of(this.topic))
+                .build();
+    }
+    public SpeakingExam toSpeakingExam(){
+        return SpeakingExam.builder()
+                .id(this.id)
+                .partOne(this.questionsOne.stream()
+                        .map(q -> IdQuestion.builder()
+                                .number(q.getNumber())
+                                .topic(this.topic)
+                                .question(q.getQuestion())
+                                .build())
+                        .toList())
+                .partTwo(IdQuestion.builder()
+                        .number(1)
+                        .topic(this.questionsTwo)
+                        .question(this.questionsTwo)
+                        .build())
+                .partThree(this.questionsThree.stream()
+                        .map(q -> IdQuestion.builder()
+                                .number(q.getNumber())
+                                .topic(this.topic)
+                                .question(q.getQuestion())
+                                .build())
+                        .toList())
+                .build();
+    }
+
 
 }
