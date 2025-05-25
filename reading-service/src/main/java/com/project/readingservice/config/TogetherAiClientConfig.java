00611d11577aa9f3ee054215ee08965922a0eb2b
tientu <tientu2003@gmail.com>
2025-05-26 00:00:44 +0700

    fix-bug Together AI

--- reading-service/src/main/java/com/project/readingservice/config/TogetherAiClientConfig.java
+++ reading-service/src/main/java/com/project/readingservice/config/TogetherAiClientConfig.java
@@ -1,8 +1,10 @@
 package com.project.readingservice.config;
 
 import feign.RequestInterceptor;
+import jakarta.ws.rs.core.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 
 public class TogetherAiClientConfig {
 
@@ -13,6 +15,7 @@ public class TogetherAiClientConfig {
     public RequestInterceptor togetherAiFeignRequestInterceptor() {
         return requestTemplate -> {
             requestTemplate.header("Authorization", "Bearer " + apiKey);
+            requestTemplate.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         };
     }
 }
